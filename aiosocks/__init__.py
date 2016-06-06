d2b082c31b4005c8a26c32036666850980951865
nibrag <ibragwork@gmail.com>
2016-06-06 13:04:46 +0300

    Bump version

--- aiosocks/__init__.py
+++ aiosocks/__init__.py
@@ -9,7 +9,7 @@ from .helpers import (
 )
 from .protocols import Socks4Protocol, Socks5Protocol, DEFAULT_LIMIT
 
-__version__ = '0.1.4'
+__version__ = '0.1.5'
 
 __all__ = ('Socks4Protocol', 'Socks5Protocol', 'Socks4Auth',
            'Socks5Auth', 'Socks4Addr', 'Socks5Addr', 'HttpProxyAddr',
