83ab4097969e3d386cb3e24aa26f28c4d9cf051f
nibrag <ibragwork@gmail.com>
2017-04-15 19:57:36 +0300

    Check python 3.5 in setup.py

--- setup.py
+++ setup.py
@@ -19,8 +19,8 @@ with codecs.open(os.path.join(os.path.abspath(os.path.dirname(
         raise RuntimeError('Unable to determine version.')
 
 
-if sys.version_info < (3, 4, 0):
-    raise RuntimeError("aiosocks requires Python 3.4+")
+if sys.version_info < (3, 5, 0):
+    raise RuntimeError("aiosocks requires Python 3.5+")
 
 
 setup(
